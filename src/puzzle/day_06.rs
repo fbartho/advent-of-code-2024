eb05a6fe321e8ed1977f25f7d35f756e615cfc85
Frederic Barthelemy <git@fbartho.com>
2024-12-11 16:46:41 -0800

    Day 06: Part 2

--- src/puzzle/day_06.rs
+++ src/puzzle/day_06.rs
@@ -21,14 +21,48 @@ impl Puzzle for Day06 {
 		let path_to_exit = the_map.exit_map();
 		println!("{}", path_to_exit.to_string());
 
-		Ok(path_to_exit
-			.find_iter(CellValue::Visited)
-			.count()
-			.to_string())
+		Ok(path_to_exit.find_any_visited().count().to_string())
 	}
 
 	fn part_two(&self, _input: &str) -> super::PuzzleResult {
-		todo!("implement part two")
+		let the_map = FBGrid::from_str(_input);
+		println!("{}", the_map.to_string());
+
+		let (exit_map, path_to_exit) = the_map
+			.exit_map_with_loops(true)
+			.expect("Couldn't find an exit without modifications");
+		println!("{}", exit_map.to_string());
+
+		let start = path_to_exit[0].0;
+		let mut unique_locations: std::collections::HashSet<GridCoord2> =
+			std::collections::HashSet::new();
+		let candidates = path_to_exit.iter().filter_map(|(loc, dir)| {
+			let mut modified_map = the_map.clone();
+			if let Some(next_loc) = dir.next_coord(*loc) {
+				if unique_locations.contains(&next_loc) {
+					return None;
+				}
+				unique_locations.insert(next_loc);
+				if let Some(current) = exit_map.grid.get(next_loc.0, next_loc.1) {
+					if next_loc != start && current.can_insert_obstacle(*dir) {
+						modified_map.grid[next_loc] = CellValue::Obstacle;
+						return Some((next_loc, modified_map));
+					}
+				}
+			}
+			return None;
+		});
+
+		let maps_that_loop: Vec<(GridCoord2, FBGrid<CellValue>)> = candidates
+			.filter_map(|(loc, modified_map)| {
+				if modified_map.exit_map_with_loops(false).is_none() {
+					return Some((loc, modified_map));
+				}
+				return None;
+			})
+			.collect();
+
+		Ok(maps_that_loop.len().to_string())
 	}
 }
 
@@ -112,24 +146,14 @@ where
 	grid: Grid<T>,
 }
 
-impl<T> FBGrid<T>
-where
-	T: Default + PartialEq,
-{
-	fn find_iter(&self, needle: T) -> impl Iterator<Item = ((usize, usize), &T)> {
-		self.grid
-			.indexed_iter()
-			.filter(move |(_, val)| **val == needle)
-	}
-}
-#[derive(Debug, Copy, Clone, PartialEq, EnumString, Display, Default)]
+#[derive(Debug, Clone, PartialEq, EnumString, Display, Default)]
 enum SerializedCellValue {
 	#[strum(serialize = ".")]
 	#[default]
 	Unknown,
 
-	#[strum(serialize = "X")]
-	Visited,
+	#[strum(serialize = "{0}")]
+	Visited(String),
 	#[strum(serialize = "#")]
 	Obstacle,
 	#[strum(serialize = "^")]
@@ -146,7 +170,12 @@ enum SerializedCellValue {
 enum CellValue {
 	#[default]
 	Unknown,
-	Visited,
+	VisitedTraveling {
+		n: bool,
+		e: bool,
+		s: bool,
+		w: bool,
+	},
 	Obstacle,
 	PlayerFacing(TravelDirection),
 }
@@ -154,12 +183,12 @@ impl From<SerializedCellValue> for CellValue {
 	fn from(value: SerializedCellValue) -> Self {
 		match value {
 			SerializedCellValue::Unknown => CellValue::Unknown,
-			SerializedCellValue::Visited => CellValue::Visited,
 			SerializedCellValue::Obstacle => CellValue::Obstacle,
 			SerializedCellValue::PlayerFacingNorth => CellValue::PlayerFacing(TravelDirection::N),
 			SerializedCellValue::PlayerFacingEast => CellValue::PlayerFacing(TravelDirection::E),
 			SerializedCellValue::PlayerFacingWest => CellValue::PlayerFacing(TravelDirection::S),
 			SerializedCellValue::PlayerFacingSouth => CellValue::PlayerFacing(TravelDirection::W),
+			_ => panic!("Unimplemented parsing map for symbol {:?}", value),
 		}
 	}
 }
@@ -167,7 +196,39 @@ impl From<CellValue> for SerializedCellValue {
 	fn from(value: CellValue) -> Self {
 		match value {
 			CellValue::Unknown => SerializedCellValue::Unknown,
-			CellValue::Visited => SerializedCellValue::Visited,
+			CellValue::VisitedTraveling { n, e, s, w } => match (n, e, s, w) {
+				// (true, true, true, true) => SerializedCellValue::Visited(String::from("✛")),
+				// (true, true, true, false) => SerializedCellValue::Visited(String::from("├")),
+				// (true, true, false, true) => SerializedCellValue::Visited(String::from("┴")),
+				// (true, true, false, false) => SerializedCellValue::Visited(String::from("└")),
+				// (true, false, true, true) => SerializedCellValue::Visited(String::from("┤")),
+				// (true, false, true, false) => SerializedCellValue::Visited(String::from("↕")),
+				// (true, false, false, true) => SerializedCellValue::Visited(String::from("┘")),
+				// (true, false, false, false) => SerializedCellValue::Visited(String::from("↑")),
+				// (false, true, true, true) => SerializedCellValue::Visited(String::from("┬")),
+				// (false, true, true, false) => SerializedCellValue::Visited(String::from("┌")),
+				// (false, true, false, true) => SerializedCellValue::Visited(String::from("↔")),
+				// (false, true, false, false) => SerializedCellValue::Visited(String::from("→")),
+				// (false, false, true, true) => SerializedCellValue::Visited(String::from("┐")),
+				// (false, false, true, false) => SerializedCellValue::Visited(String::from("↓")),
+				// (false, false, false, true) => SerializedCellValue::Visited(String::from("←")),
+				(true, true, true, true) => SerializedCellValue::Visited(String::from("✛")),
+				(true, true, true, false) => SerializedCellValue::Visited(String::from("✛")),
+				(true, true, false, true) => SerializedCellValue::Visited(String::from("✛")),
+				(true, true, false, false) => SerializedCellValue::Visited(String::from("✛")),
+				(true, false, true, true) => SerializedCellValue::Visited(String::from("✛")),
+				(true, false, true, false) => SerializedCellValue::Visited(String::from("↕")),
+				(true, false, false, true) => SerializedCellValue::Visited(String::from("✛")),
+				(true, false, false, false) => SerializedCellValue::Visited(String::from("↑")),
+				(false, true, true, true) => SerializedCellValue::Visited(String::from("✛")),
+				(false, true, true, false) => SerializedCellValue::Visited(String::from("✛")),
+				(false, true, false, true) => SerializedCellValue::Visited(String::from("↔")),
+				(false, true, false, false) => SerializedCellValue::Visited(String::from("→")),
+				(false, false, true, true) => SerializedCellValue::Visited(String::from("✛")),
+				(false, false, true, false) => SerializedCellValue::Visited(String::from("↓")),
+				(false, false, false, true) => SerializedCellValue::Visited(String::from("←")),
+				(false, false, false, false) => SerializedCellValue::Unknown,
+			},
 			CellValue::Obstacle => SerializedCellValue::Obstacle,
 			CellValue::PlayerFacing(dir) => match dir {
 				TravelDirection::N => SerializedCellValue::PlayerFacingNorth,
@@ -176,6 +237,23 @@ impl From<CellValue> for SerializedCellValue {
 				TravelDirection::W => SerializedCellValue::PlayerFacingSouth,
 				_ => panic!("Unimplemented player direction"),
 			},
+		}
+	}
+}
+impl CellValue {
+	/// We can only add an obstacle to Visited tiles in the exit map
+	fn can_insert_obstacle(&self, dir: TravelDirection) -> bool {
+		match self {
+			CellValue::Unknown => panic!("Useless to add an obstacle off the path"),
+			CellValue::VisitedTraveling { n, e, s, w } => match dir {
+				TravelDirection::N => n == &(dir == TravelDirection::N),
+				TravelDirection::E => e == &(dir == TravelDirection::E),
+				TravelDirection::S => s == &(dir == TravelDirection::S),
+				TravelDirection::W => w == &(dir == TravelDirection::W),
+				_ => false,
+			},
+			CellValue::Obstacle => false,
+			CellValue::PlayerFacing(_) => false,
 		}
 	}
 }
@@ -210,11 +288,68 @@ impl FBGrid<CellValue> {
 		}
 		return None;
 	}
+	fn find_any_visited(&self) -> impl Iterator<Item = (GridCoord2, &CellValue)> {
+		self.grid.indexed_iter().filter(move |(_, val)| match val {
+			CellValue::VisitedTraveling { .. } => true,
+			_ => false,
+		})
+	}
+	fn visited_for(val: CellValue, dir: TravelDirection) -> Result<CellValue, ()> {
+		let next_is_n = TravelDirection::N == dir;
+		let next_is_e = TravelDirection::E == dir;
+		let next_is_s = TravelDirection::S == dir;
+		let next_is_w = TravelDirection::W == dir;
+
+		match val {
+			CellValue::VisitedTraveling { n, e, s, w } => {
+				if n && next_is_n || e && next_is_e || s && next_is_s || w && next_is_w {
+					// Already had marked a direction so this is a loop!
+					return Result::Err(());
+				}
+				return Result::Ok(CellValue::VisitedTraveling {
+					n: n || next_is_n,
+					e: e || next_is_e,
+					s: s || next_is_s,
+					w: w || next_is_w,
+				});
+			}
+			CellValue::Obstacle => panic!("Cannot go through an obstacle!"),
+			_ => Result::Ok(CellValue::VisitedTraveling {
+				n: TravelDirection::N == dir,
+				e: TravelDirection::E == dir,
+				s: TravelDirection::S == dir,
+				w: TravelDirection::W == dir,
+			}),
+		}
+	}
+	/// Errs if it detects a loop!
+	fn add_travel_direction_to_cell(
+		&mut self,
+		loc: GridCoord2,
+		dir: TravelDirection,
+	) -> Result<(), ()> {
+		match FBGrid::visited_for(self.grid[loc], dir) {
+			Ok(val) => {
+				self.grid[loc] = val;
+				return Result::Ok(());
+			}
+			Err(e) => Result::Err(e),
+		}
+	}
 	/// Create a new map where we've marked all the paths we'd take to exit the maze
-	fn exit_map(&self) -> Self {
+	/// -- Abort if we detect a loop
+	fn exit_map_with_loops(
+		&self,
+		collect_path: bool,
+	) -> Option<(Self, Vec<(GridCoord2, TravelDirection)>)> {
+		// println!("{}", self.to_string());
 		let start = self.find_start().expect("No starting location found");
 		let mut result = self.clone();
 		let mut current = start;
+		let mut path: Vec<(GridCoord2, TravelDirection)> = Vec::new();
+		if collect_path {
+			path.push(current);
+		}
 		while let Some(next_loc) = current.1.next_coord(current.0) {
 			// println!(
 			// 	"{:?} {}:\n {}",
@@ -224,26 +359,40 @@ impl FBGrid<CellValue> {
 			// );
 			// println!("{}", result.to_string());
 
-			result.grid[current.0] = CellValue::Visited;
+			if let Result::Err(_e) = result.add_travel_direction_to_cell(current.0, current.1) {
+				// Loop detected!
+				return None;
+			}
+			if collect_path {
+				path.push(current);
+			}
 			if let Some(next_cell) = self.grid.get(next_loc.0, next_loc.1) {
 				match next_cell {
 					CellValue::Obstacle => {
 						let next_dir = current.1.rt90();
 						current.1 = next_dir
 					}
-					// Just advance forwards if it's not an obstacle
-					_ => current.0 = next_loc,
+					_ => {
+						// advance forwards if it's not an obstacle
+						current.0 = next_loc
+					}
 				}
 			} else {
 				// We found an edge! (E, S)
 				break;
 			}
 		}
-		// When you exit off the top/left, we don't enter the loop and mark the cell as visited
-		result.grid[current.0] = CellValue::Visited;
+		// When you exit off the top/left, we don't mark the cell as visited
+		let _ = result.add_travel_direction_to_cell(current.0, current.1);
+		if collect_path {
+			path.push(current);
+		}
 
 		println!("last: {:?}", current);
-		return result;
+		return Some((result, path));
+	}
+	fn exit_map(&self) -> Self {
+		return self.exit_map_with_loops(false).expect("No exit found").0;
 	}
 	fn to_string(&self) -> String {
 		// let gs = self.grid.to_string()
@@ -279,4 +428,21 @@ fn sample_day06_1() {
 ......#...
 "#;
 	println!("{:?}", Day06.part_one(input).unwrap());
+}
+
+#[test]
+fn sample_day06_2() {
+	let input = r#"
+....#.....
+.........#
+..........
+..#.......
+.......#..
+..........
+.#..^.....
+........#.
+#.........
+......#...
+"#;
+	println!("{:?}", Day06.part_two(input).unwrap());
 }
